3cff6371a983d90f2e55f0a6105cc0832caebf2f
bosiarquitetura <bosiarquitetura@gmail.com>
2019-12-08 10:41:02 -0300

    GitHub Pages
    Deploy do service worker

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "static-cache-v1.2";
-const DATA_CACHE_NAME = "data-cache-v1.2";
+const CACHE_NAME = "static-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
 
 const FILES_TO_CACHE = [
   "index.html",
