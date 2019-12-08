d9d050ef66c024fb9cdc7b1c5ab23699c15d0717
bosiarquitetura <bosiarquitetura@gmail.com>
2019-12-08 16:12:19 -0300

    Versão final do front
    Teste da versão final do front e upload da análise de dados

--- docs/js/questions.js
+++ docs/js/questions.js
@@ -1,24 +1,56 @@
 const questions = [
-  { id: 1, question: "Você dormiu bem?" },
-  { id: 2, question: "Pergunta teste?" },
-  { id: 3, question: "Pergunta teste2?" },
-  { id: 4, question: "Pergunta teste3?" },
-  { id: 5, question: "Pergunta teste4?" },
-  { id: 6, question: "Pergunta teste5?" },
-  { id: 7, question: "Pergunta teste6?" },
-  { id: 8, question: "Pergunta teste7?" },
-  { id: 9, question: "Pergunta teste8?" },
-  { id: 10, question: "Pergunta teste9?" }
+  { id: 1, question: "Você dormiu na noite/dia ontem?" },
+  {
+    id: 2,
+    question:
+      "Você está sentindo alguma dor ao caminhar, se abaixar ou pegar algum peso?"
+  },
+  { id: 3, question: "Você bebeu alguma cervejinha ontem?" },
+  {
+    id: 4,
+    question:
+      "Você está usando algum medicamento controlado (remédio pra dormir, calmante, etc.)?"
+  },
+  { id: 5, question: "Você está se sentindo estressado ou agitado??" },
+  {
+    id: 6,
+    question:
+      "Você está se sentindo triste ou deprimido hoje ou nos últimos tempos?"
+  },
+  { id: 7, question: "Está tudo bem com você e com sua família?" },
+  {
+    id: 8,
+    question:
+      "Você está tendo dificuldades na atividade que executa no trabalho ou com seus colegas?"
+  },
+  { id: 9, question: "Você está com dificuldades financeiras?" },
+  { id: 10, question: "Você está sentindo algum mal estar?" }
 ];
 const conditionalQuestions = [
   {
-    id: 1,
-    question: ""
+    id: 11,
+    question: "Clonazepam?"
+  },
+  {
+    id: 12,
+    question: "Fenobarbital?"
   }
 ];
 
 const answers = [];
 let questionCount = 1;
+let conditionalCount = 11;
+
+function getQuestionById(id) {
+  const selectedQuestion = questions.find(question => question.id === id);
+  question.innerHTML = selectedQuestion.question;
+}
+function getConditionalQuestionById(id) {
+  const selectedConditionalQuestion = conditionalQuestions.find(
+    question => question.id === id
+  );
+  question.innerHTML = selectedConditionalQuestion.question;
+}
 
 function nextQuestion(answer) {
   const singularAnswer = {
@@ -27,6 +59,15 @@ function nextQuestion(answer) {
     answer: answer
   };
   answers.push(singularAnswer);
+
+  if (
+    (questionCount === 4 && answer === "Sim" && conditionalCount === 11) ||
+    conditionalCount == 12
+  ) {
+    getConditionalQuestionById(conditionalCount);
+    conditionalCount = conditionalCount + 1;
+    return;
+  }
 
   questionCount = questionCount + 1;
 
